642660bc27261920c85b1071f4bb5b18cd3c5687
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2025-09-08 10:36:45 +0530

    Update cassConnectionManager.py

--- cassConnectionManager.py
+++ cassConnectionManager.py
@@ -7,7 +7,7 @@ from cassandra.query   import SimpleStatement
 from globalSettings    import *
 
 ### connection variables for on-prem Cassandra or DSE
-CASS_CONTACT_POINTS    = ["57.180.243.169", "18.183.235.247","3.94.100.117", "3.93.177.198"] ;
+CASS_CONTACT_POINTS    = ["13.221.91.243", "18.183.188.248","13.221.91.243", "98.81.78.89"] ;
 CASS_PORT              = 9042 ;
 CASS_USERNAME          = "sales_user" ;
 CASS_PASSWORD          = "PassCode" ;
